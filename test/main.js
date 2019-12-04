6c61a23bf433bc586fa9b95a78537e69622bcc32
fisker <lionkay@gmail.com>
2019-12-04 15:53:52 +0800

    test: test help

--- test/main.js
+++ test/main.js
@@ -1,16 +1,16 @@
 import test from 'ava'
 import execa from 'execa'
-import {version} from '../package.json'
+import {version, description} from '../package.json'
 
 const runCLI = cliArguments =>
   execa('node', ['-r', 'esm', 'src/index.js', ...cliArguments])
 
-// test('cli', async t => {
-//   const {stdout} = await runCLI(['--version'])
-//   t.is(stdout, version)
-// })
-
 test('--version', async t => {
   const {stdout} = await runCLI(['--version'])
   t.is(stdout, version)
+})
+
+test('--help', async t => {
+  const {stdout} = await runCLI(['--help'])
+  t.true(stdout.includes(description))
 })
