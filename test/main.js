b498aaa6a28c566d535c26fe6967122fb6282c46
fisker Cheung <lionkay@gmail.com>
2019-12-04 18:43:46 +0800

    test: add test (#179)

---
+++ test/main.js
@@ -0,0 +1,16 @@
+import test from 'ava'
+import execa from 'execa'
+import {version, description} from '../package.json'
+
+const runCLI = cliArguments =>
+  execa('node', ['-r', 'esm', 'src/index.js', ...cliArguments])
+
+test('--version', async t => {
+  const {stdout} = await runCLI(['--version'])
+  t.is(stdout, version)
+})
+
+test('--help', async t => {
+  const {stdout} = await runCLI(['--help'])
+  t.true(stdout.includes(description))
+})
