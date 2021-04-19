5554e8f7a19fbd7ef5967c9ff6b3e8584ec5311c
fisker Cheung <lionkay@gmail.com>
2021-04-19 16:30:00 +0800

    Refactor

--- src/load-package-json.js
+++
@@ -1,9 +0,0 @@
-import {promises as fs} from 'fs'
-
-async function loadPackageJson() {
-  const packageJson = new URL('../package.json', import.meta.url)
-  const text = await fs.readFile(packageJson, 'utf8')
-  return JSON.parse(text)
-}
-
-export default loadPackageJson
--- test/main.js
+++ test/main.js
@@ -1,18 +1,21 @@
+import path from 'path'
 import test from 'ava'
 import execa from 'execa'
-import loadPackageJson from '../src/load-package-json.js'
+import createEsmUtils from 'esm-utils'
+
+const {json, dirname} = createEsmUtils(import.meta)
 
 const runCLI = (cliArguments) =>
-  execa('node', ['src/index.js', ...cliArguments])
+  execa('node', [path.join(dirname, '../cli.js'), ...cliArguments])
 
 test('--version', async (t) => {
-  const {version} = await loadPackageJson()
+  const {version} = await json.load('../package.json')
   const {stdout} = await runCLI(['--version'])
   t.is(stdout, version)
 })
 
 test('--help', async (t) => {
-  const {description} = await loadPackageJson()
+  const {description} = await json.load('../package.json')
   const {stdout} = await runCLI(['--help'])
   t.true(stdout.includes(description))
 })
