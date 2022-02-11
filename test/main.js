80462543bf32e6f54b343f6fa1f07b48ee209eb4
fisker Cheung <lionkay@gmail.com>
2022-02-11 16:42:14 +0800

    Update dependencies

--- test/main.js
+++ test/main.js
@@ -3,19 +3,19 @@ import test from 'ava'
 import {execa} from 'execa'
 import createEsmUtils from 'esm-utils'
 
-const {json, dirname} = createEsmUtils(import.meta)
+const {readJson, dirname} = createEsmUtils(import.meta)
 
 const runCLI = (cliArguments) =>
   execa('node', [path.join(dirname, '../cli.js'), ...cliArguments])
 
 test('--version', async (t) => {
-  const {version} = await json.load('../package.json')
+  const {version} = await readJson('../package.json')
   const {stdout} = await runCLI(['--version'])
   t.is(stdout, version)
 })
 
 test('--help', async (t) => {
-  const {description} = await json.load('../package.json')
+  const {description} = await readJson('../package.json')
   const {stdout} = await runCLI(['--help'])
   t.true(stdout.includes(description))
 })
